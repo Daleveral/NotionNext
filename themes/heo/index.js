4c206fae3f7bdd3bcf14a1fe7f823c336c71a226
Dale <2448926122@qq.com>
2023-08-11 17:43:06 +0800

    comments logo

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -255,7 +255,7 @@ const LayoutSlug = props => {
 
                     {/* 评论互动 */}
                     <div className="duration-200 overflow-x-auto px-5">
-                        <div className='text-2xl dark:text-white'><i className='fas fa-comment mr-1' />{locale.COMMON.COMMENTS}</div>
+                        <div className='text-2xl dark:text-white'><i className='far fa-comment-dots' />{locale.COMMON.COMMENTS}</div>
                         <Comment frontMatter={post} className='' />
                     </div>
                 </div>}
