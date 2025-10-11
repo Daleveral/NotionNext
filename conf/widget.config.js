e8ba6a54931fd958ca55399675eacc93364e2397
DaleChu <ringingtrue7@gmail.com>
2025-10-12 00:53:39 +0800

    more more

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -37,18 +37,300 @@ module.exports = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
-      cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+          name: 'なんでもないや',
+          artist: 'Akie秋绘',
+          url: 'http://music.163.com/song/media/outer/url?id=450795499.mp3',
+          cover: '/images/aplayer.png'
     },
     {
-      name: '王都グランセル',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
-      cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+          name: '风を共に舞う気持ち',
+          artist: 'Falcom Sound Team jdk',
+          url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '王都グランセル',
+          artist: 'Falcom Sound Team jdk',
+          url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Bohemian Rhapsody',
+          artist: 'Queen',
+          url: 'http://music.163.com/song/media/outer/url?id=1868553.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'On a Slow Boat to China',
+          artist: 'Luke Thompson',
+          url: 'http://music.163.com/song/media/outer/url?id=27591140.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Liekkas',
+          artist: 'Sofia Jannok',
+          url: 'http://music.163.com/song/media/outer/url?id=3413895.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Only Time',
+          artist: 'Enya',
+          url: 'http://music.163.com/song/media/outer/url?id=29470947.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: "My Love",
+          artist: 'Westlife',
+          url: 'http://music.163.com/song/media/outer/url?id=2081191.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: "In the End",
+          artist: 'Linkin Park',
+          url: 'http://music.163.com/song/media/outer/url?id=16686470.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'You',
+          artist: 'Approaching Nirvana',
+          url: 'http://music.163.com/song/media/outer/url?id=1891437743.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'In My Life',
+          artist: 'The Beatles',
+          url: 'http://music.163.com/song/media/outer/url?id=1476058.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Beautiful',
+          artist: 'Virginia To Vegas',
+          url: 'http://music.163.com/song/media/outer/url?id=29722661.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'We Are Young',
+          artist: 'Fun, Janelle Monáe',
+          url: 'http://music.163.com/song/media/outer/url?id=28329964.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'That Girl',
+          artist: 'Olly Murs',
+          url: 'http://music.163.com/song/media/outer/url?id=864650750.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '忽然之间',
+          artist: '莫文蔚',
+          url: 'http://music.163.com/song/media/outer/url?id=27937466.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'You Raise Me Up',
+          artist: 'Westlife',
+          url: 'http://music.163.com/song/media/outer/url?id=20707713.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Mr.Moustafa',
+          artist: 'Alexandre Desplat',
+          url: 'http://music.163.com/song/media/outer/url?id=28289088.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'What Are Words',
+          artist: 'Chris Medina',
+          url: 'http://music.163.com/song/media/outer/url?id=1210496.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Take Me Home Country Roads',
+          artist: 'John Denver',
+          url: 'http://music.163.com/song/media/outer/url?id=27448352.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Color Blind',
+          artist: 'Matt B',
+          url: 'http://music.163.com/song/media/outer/url?id=1910991521.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'New Soul',
+          artist: 'Yael Naim',
+          url: 'http://music.163.com/song/media/outer/url?id=2010944681.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'arhbo ( 球员入阵 )',
+          artist: 'Gims/Ozuna',
+          url: 'http://music.163.com/song/media/outer/url?id=2042622526.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '莫斯科郊外的晚上',
+          artist: '中央音乐合唱团',
+          url: 'http://music.163.com/song/media/outer/url?id=5280398.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Sound of Silence',
+          artist: 'Simon & Garfunkel',
+          url: 'http://music.163.com/song/media/outer/url?id=491424530.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Hayya Hayya',
+          artist: 'Trinidad Cardona,DaVido,Aisha',
+          url: 'http://music.163.com/song/media/outer/url?id=1933800693.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'What Makes You Beautiful',
+          artist: 'One Direction',
+          url: 'http://music.163.com/song/media/outer/url?id=27218979.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '海阔天空',
+          artist: 'Beyond',
+          url: 'http://music.163.com/song/media/outer/url?id=1357374736.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '喜欢你',
+          artist: 'Beyond',
+          url: 'http://music.163.com/song/media/outer/url?id=346163.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Go West',
+          artist: 'Pet Shop Boys',
+          url: 'http://music.163.com/song/media/outer/url?id=27198266.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Iridescent',
+          artist: 'Linkin Park',
+          url: 'http://music.163.com/song/media/outer/url?id=25639052.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Komorebi',
+          artist: 'm-taku',
+          url: 'http://music.163.com/song/media/outer/url?id=29803287.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '亡命之徒',
+          artist: '纵贯线',
+          url: 'http://music.163.com/song/media/outer/url?id=5237863.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '起风了',
+          artist: '买辣椒也用券',
+          url: 'http://music.163.com/song/media/outer/url?id=1330348068.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '小城大事',
+          artist: '杨千嬅',
+          url: 'http://music.163.com/song/media/outer/url?id=1357379611.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Here with You',
+          artist: 'Asher Monroe',
+          url: 'http://music.163.com/song/media/outer/url?id=27583305.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Imagine',
+          artist: 'John Lennon',
+          url: 'http://music.163.com/song/media/outer/url?id=1476431.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Hey Jude',
+          artist: 'The Beatles',
+          url: 'http://music.163.com/song/media/outer/url?id=5201810.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Summer',
+          artist: '久石让',
+          url: 'http://music.163.com/song/media/outer/url?id=1867107328.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Yellow',
+          artist: 'Coldplay',
+          url: 'http://music.163.com/song/media/outer/url?id=26807310.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '理想三旬',
+          artist: '陈鸿宇',
+          url: 'http://music.163.com/song/media/outer/url?id=31445772.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '北京东路的日子', 
+          artist: '群星',
+          url: 'http://music.163.com/song/media/outer/url?id=458245372.mp3', 
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: '愿得一人心',
+          artist: '李行亮',
+          url: 'http://music.163.com/song/media/outer/url?id=25731646.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name:  '七月上',
+          artist: 'Jam',
+          url: 'http://music.163.com/song/media/outer/url?id=31445554.mp3',
+          cover: '/images/aplayer.png'
+    },   
+    {
+          name:  'Dont Break My Heart',
+          artist: '黑豹乐队',
+          url: 'http://music.163.com/song/media/outer/url?id=1859327.mp3',
+          cover: '/images/aplayer.png'
+    },   
+    {
+          name: 'Last First Kiss',
+          artist: 'One Direction',
+          url: 'http://music.163.com/song/media/outer/url?id=25882975.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Here Comes the Sun',
+          artist: 'The Beatles',
+          url: 'http://music.163.com/song/media/outer/url?id=28100002.mp3',
+          cover: '/images/aplayer.png'
+    },
+
+    {
+          name: '水星记',
+          artist: '郭顶',
+          url: 'http://music.163.com/song/media/outer/url?id=441491828.mp3',
+          cover: '/images/aplayer.png'
+    },
+
+    {
+          name: 'Look At Me Now',
+          artist: 'Charlie Puth',
+          url: 'http://music.163.com/song/media/outer/url?id=28188171.mp3',
+          cover: '/images/aplayer.png'
+    },
+    {
+          name: 'Brought Up That Way',
+          artist: 'Taylor Swift',
+          url: 'http://music.163.com/song/media/outer/url?id=2001660.mp3',
+          cover: '/images/aplayer.png'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
